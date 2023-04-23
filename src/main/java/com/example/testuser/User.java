4761d6c549890e2a66b4df1eb9639d3b7021ab00
Tsiri <60728565+dnv0@users.noreply.github.com>
2023-04-24 01:26:17 +0700

    Commit #1

---
+++ src/main/java/com/example/testuser/User.java
@@ -0,0 +1,53 @@
+package com.example.testuser;
+
+import java.util.Objects;
+
+public class User {
+    private String email;
+    private String login;
+
+    public User(String email, String login) {
+        this.email = email;
+        this.login = login;
+    }
+
+    public static boolean validationTwoUserParameters (User user) {
+        return !user.getEmail().isBlank() && !user.getLogin().isBlank();
+    }
+
+    public static boolean validationWithoutUserParameters(User user) {
+        return user.getEmail() == null && user.getLogin() == null;
+    }
+
+    public static boolean validationEmailDogAt(User user) {
+        return user.getEmail().contains("@") && user.getEmail().contains(".");
+    }
+
+    public static boolean equalsEmailAndLogin(User user) {
+        return user.getEmail().equals(user.getLogin());
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) && Objects.equals(login, user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, login);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
---
+++ src/test/java/com/example/testuser/UserTest.java
@@ -0,0 +1,41 @@
+package com.example.testuser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    private static String email = "user@gmail.ru";
+    private static String login = "qwerty";
+
+//    private static String email = null;
+//    private static String login = null;
+
+    private final User userTest = new User(email, login);
+
+    @Test
+    @DisplayName("Тестирование создания объекта User с передачей в него двух параметров")
+    void shouldValidationTwoUserParameters() {
+        Assertions.assertTrue(User.validationTwoUserParameters(userTest));
+    }
+
+    @Test
+    @DisplayName("Тестирование по созданию объекта User без передачи в него параметров")
+    void shouldValidationWithoutUserParameters() {
+        Assertions.assertTrue(User.validationWithoutUserParameters(userTest));
+    }
+
+    @Test
+    @DisplayName("Тестирование корректного Email адрес в поле email в классе User")
+    void shouldValidationEmailDogAt() {
+        Assertions.assertTrue(User.validationEmailDogAt(userTest));
+    }
+
+    @Test
+    @DisplayName("Тест, который определяет, равны ли login и email")
+    void shouldEqualsEmailAndLogin() {
+        Assertions.assertFalse(User.equalsEmailAndLogin(userTest));
+    }
+}
